531c4c329ef9f0a1789fc49b5bb3a243dd350160
nickdapang <nickdapang@gmail.com>
2025-01-27 19:39:33 -0500

    complete

--- hws/hw1/main.py
+++ hws/hw1/main.py
@@ -4,12 +4,39 @@ from weather_analysis import *
 
 def weather_analyze(file_path):
     dict = {}
-
+    dict1 = {}
+    dict2 = {}
+    dict3 = {}
     list = read_weather_data(file_path)
     
     avg_temp = calculate_average_temperature(list)
 
     dict['average_temperature'] = avg_temp
+
+    total_rainfall = calculate_total_rainfall(list)
+
+    dict['total_rainfall'] = total_rainfall
+
+    high_temp = find_highest_temperature(list)
+
+    dict1['date'] = high_temp[1]
+    dict1['temperature'] = high_temp[0]
+
+
+    dict['highest_temperature'] = (dict1)
+
+    low_temp = find_lowest_temperature(list)
+     
+    dict2['date'] = low_temp[1]
+    dict2['temperature'] = low_temp[0]
+
+    dict['lowest_temperature'] = (dict2)
+
+    most_rainfall = find_day_with_most_rainfall(list)
+
+    dict3['date'] = most_rainfall[1]
+    dict3['rainfall'] = most_rainfall[0]
+    dict['most_rainfall'] = (dict3)
 
     return dict
 
--- hws/hw1/weather_analysis.py
+++ hws/hw1/weather_analysis.py
@@ -21,29 +21,32 @@ def calculate_average_temperature(weather_data):
 def calculate_total_rainfall(weather_data):
     total = 0
     for i in weather_data:
-        total += i[2]
+        total += (float)(i[2])
     return total
 
 def find_highest_temperature(weather_data):
     highest_temp = -99999
     
     for i in weather_data:
-        if i[1] > highest_temp:
-            highest_temp = i[1]
-    return highest_temp
+        if int(i[1]) > highest_temp:
+            highest_temp = int(i[1])
+            highest_date = i[0]
+    return highest_temp, highest_date
 
 
 def find_lowest_temperature(weather_data):
     lowest_temp = 9999999999
     for i in weather_data:
-        if i[1] < lowest_temp:
-            lowest_temp = i[1]
-    return lowest_temp
+        if int(i[1]) < lowest_temp:
+            lowest_temp = int(i[1])
+            lowest_date = str(i[0])
+    return lowest_temp, lowest_date
 
 def find_day_with_most_rainfall(weather_data):
     highest_rainfall = 0
     for i in weather_data:
-        if i[2] > highest_rainfall:
-            highest_rainfall = i[2]
-    return highest_rainfall
+        if float(i[2]) > highest_rainfall:
+            highest_rainfall = float(i[2])
+            highrainfall_date = str(i[0])
+    return highest_rainfall, highrainfall_date
 
